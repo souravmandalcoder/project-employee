ae4bb6705a4aee8127c2820439c77ceaa6f0b060
Sourav <souravmandal983@gmail.com>
2022-06-28 13:30:29 +0530

    changed login form

--- src/app/core/material-library.module.ts
+++ src/app/core/material-library.module.ts
@@ -5,13 +5,17 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 const modules = [
   MatInputModule,
   MatFormFieldModule,
   MatButtonModule,
   MatIconModule,
-  MatTableModule
+  MatTableModule,
+  MatCardModule,
+  MatToolbarModule
 ]
 
 @NgModule({
